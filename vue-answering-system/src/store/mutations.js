3dd3dca79192365d08513a1c44b7a8431a0e6e80
Jonnyy <caijishiwo@gmail.com>
2019-04-17 20:40:45 -0400

    mutations

---
+++ vue-answering-system/src/store/mutations.js
@@ -0,0 +1,11 @@
+const ADD_ITEMNUM = 'ADD_ITEMNUM'
+const REMBER_ANSWER = 'REMBER_ANSWER'
+const ADD_TIME = 'REMBER_TIME'
+const INITIALIZE_DATA = 'INITIALIZE_DATA'
+export default{
+    //click into next question
+    [ADD_ITEMNUM](state, num){
+        state.itemNum += num;
+    },
+    //record the answer
+}
